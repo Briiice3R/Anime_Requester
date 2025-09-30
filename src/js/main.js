7121ad40615f1dd6e8fba9cf66a0537cdffcc572
Pellerin <milene.pellerin@etu.unicaen.fr>
2025-09-30 15:38:48 +0200

    style : display of anime cards + search and reinitialisation buttons

--- src/js/main.js
+++ src/js/main.js
@@ -2,3 +2,22 @@ import apiHandler from "./getDataAPI.js";
 
 const d = await apiHandler.getAnimeByName("Fullmetal");
 console.log(d);
+
+
+const template = document.getElementById("anime-card");
+const container = document.getElementById("div-anime-cards");
+
+
+d.forEach(anime => {
+    console.log(anime)
+    const animeCard = template.content.cloneNode(true);
+    animeCard.querySelector('.anime-card-title').textContent = anime.title;
+    animeCard.querySelector('.anime-card-img').src = anime.image;
+    animeCard.querySelector('.anime-card-synopsis').textContent = "Synopsis : "+anime.synopsis;
+    animeCard.querySelector('.anime-card-genre').textContent = "Genre : "+anime.genres[0];
+    animeCard.querySelector('.anime-card-rank').textContent = "Rang : "+anime.rank;
+    animeCard.querySelector('.anime-card-episode-number').textContent = "Episodes : "+ anime.episodes;
+    animeCard.querySelector('.anime-card-id').textContent = "Id : "+anime._id;
+    
+    container.appendChild(animeCard);
+});
