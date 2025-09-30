dbf5a8b5fc33962bd597e171bb11a6747e2e7e44
BriceHarper <58884670+BriceHarper@users.noreply.github.com>
2025-09-30 15:11:36 +0200

    refactor: Add non-production version of api fetching

--- src/js/getDataAPI.js
+++ src/js/getDataAPI.js
@@ -2,7 +2,29 @@ const HOST = "https://anime-db.p.rapidapi.com"
 const APIKEY = await fetch("../../key.json", {method: "GET"}).then((data)=>data.json().then((data)=>data.key));
 
 
-async function fetchAPI(endpoint, params = ""){
+async function fetchAPI(endpoint = "", params = "", prod=false){
+    if(!prod){
+        try{
+            const resFile = await fetch("../../test.json", {method: "GET"}).then((data)=>data.json());
+            if(endpoint.includes("by-id")){
+                return resFile.data[0];
+            }else if(endpoint.includes("search")){
+                return resFile.data;
+
+            }else if(endpoint.includes("by-ranking")){
+                return resFile.data[0];
+                
+            } else{
+                return resFile.data;
+                
+            }
+                    
+            return 0;
+        } catch(e){
+            console.log(e);
+        }
+       
+    }
     try{
         const reqApi = await fetch(`${HOST}${endpoint}${params}`, 
         {
@@ -12,7 +34,7 @@ async function fetchAPI(endpoint, params = ""){
             }
         });
         const data = await reqApi.json();
-        return data.data;
+        return data.data || data;
 
     
     } catch(e){
@@ -21,10 +43,10 @@ async function fetchAPI(endpoint, params = ""){
 }
 
 const apiHandler = {
-    getAnimeById: id=>fetchAPI(`/anime/by-id/${id}`),
+    getAnimeById: id=>fetchAPI(`/anime/by-id/${id}`, ""),
     getAnimeByName: (name)=>fetchAPI(`/anime?search=${name}`,"&page=1&size=10"),
-    getAnimeByRank: rank => fetchAPI(`/anime/by-ranking/${rank}`),
-    getTenAnime: (sortAsc=true)=> fetchAPI("/anime", `?size=10&${sortAsc?"asc":"desc"}`)
+    getAnimeByRank: rank => fetchAPI(`/anime/by-ranking/${rank}`, ""),
+    getTenAnime: (sortAsc=true)=> fetchAPI("/anime", `?page=1&size=10&sortOrder=${sortAsc?"asc":"desc"}`)
 };
 
 export default apiHandler;
