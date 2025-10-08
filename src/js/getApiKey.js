d324093226aef322cdb507d015c0b1ad06475fcb
BriceHarper <58884670+BriceHarper@users.noreply.github.com>
2025-10-08 14:42:54 +0200

    refactor: Change system API key

--- src/js/getApiKey.js
+++ src/js/getApiKey.js
@@ -1,20 +1,31 @@
-import apiHandler from "./getDataAPI.js";
+// import apiHandler from "./getDataAPI.js";
 let cleApi;
 
 export function popUpAPI() {
     return new Promise((resolve, reject) => {
         cleApi = prompt("Veuillez entrer votre clé API :");
-
         if (cleApi) {
+            fetch("https://anime-db.p.rapidapi.com/anime?page=1&size=10&search=Fullmetal&genres=Fantasy%2CDrama&sortBy=ranking&sortOrder=asc",{
+                method: "GET",
+                headers:{
+                "x-rapidapi-key": cleApi
+                }
+            }).then(res=>{
+                if(res.ok){
+                    window.sessionStorage.setItem("APIKey",cleApi);
+                    resolve()
+                } else{
+                    reject("Clé saisie invalide❌")
+                }
+            }).catch((e)=>{
+                reject("Aucune clé saisie ❌")
+            })
             //console.log("Clé API :", cleApi);
-            resolve(cleApi)
         } else {
             reject("Aucune clé saisie ❌");
         }
     })   
 };
 
-export const ExportcleApi = cleApi;
-
-const d = await apiHandler.getAnimeByName("Fullmetal");
-console.log(d);
+// const d = await apiHandler.getAnimeByName("Fullmetal");
+// console.log(d);
--- src/js/main.js
+++ src/js/main.js
@@ -1,35 +1,35 @@
+import {popUpAPI} from "./getApiKey.js";
 import apiHandler from "./getDataAPI.js";
-import {popUpAPI, ExportcleApi} from "./getApiKey.js";
 
 async function init(){
     try{
-        const cleAPI = await popUpAPI();
-        console.log(cleAPI);
+        if(!window.sessionStorage.getItem("APIKey")){
+            await popUpAPI();
+        }
+        
         const d = await apiHandler.getAnimeByName("Fullmetal");
-        console.log(d);
+        
+        const template = document.getElementById("anime-card");
+        const container = document.getElementById("div-anime-cards");
 
 
-    const template = document.getElementById("anime-card");
-    const container = document.getElementById("div-anime-cards");
+        
+        d.forEach(anime => {
+            
+            const animeCard = template.content.cloneNode(true);
+            animeCard.querySelector('.anime-card-title').textContent = anime.title;
+            animeCard.querySelector('.anime-card-img').src = anime.image;
+            animeCard.querySelector('.anime-card-synopsis').textContent = "Synopsis : "+anime.synopsis;
+            animeCard.querySelector('.anime-card-genre').textContent = "Genre : "+anime.genres[0];
+            animeCard.querySelector('.anime-card-rank').textContent = "Rang : "+anime.rank;
+            animeCard.querySelector('.anime-card-episode-number').textContent = "Episodes : "+ anime.episodes;
+            animeCard.querySelector('.anime-card-id').textContent = "Id : "+anime._id;
+        
+            container.appendChild(animeCard);
 
-
-
-    d.forEach(anime => {
-        console.log(anime)
-        const animeCard = template.content.cloneNode(true);
-        animeCard.querySelector('.anime-card-title').textContent = anime.title;
-        animeCard.querySelector('.anime-card-img').src = anime.image;
-        animeCard.querySelector('.anime-card-synopsis').textContent = "Synopsis : "+anime.synopsis;
-        animeCard.querySelector('.anime-card-genre').textContent = "Genre : "+anime.genres[0];
-        animeCard.querySelector('.anime-card-rank').textContent = "Rang : "+anime.rank;
-        animeCard.querySelector('.anime-card-episode-number').textContent = "Episodes : "+ anime.episodes;
-        animeCard.querySelector('.anime-card-id').textContent = "Id : "+anime._id;
-    
-        container.appendChild(animeCard);
-
-    });
-    }   catch (error){
-        console.error("erreur recup clé", error);
+        });
+    }catch (error){
+        init();
     }
 }
 
