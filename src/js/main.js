3b31c8d1ec0ced4802ab4a130572e2892447c7c8
PELLERIN <milene.pellerin@etu.unicaen.fr>
2025-10-09 08:44:44 +0200

    fit : addition of searching system by title

--- src/js/main.js
+++ src/js/main.js
@@ -1,41 +1,15 @@
 import {popUpAPI} from "./getApiKey.js";
 import apiHandler from "./getDataAPI.js";
 const searchType = document.getElementById("searchType");
-const searchParameter = document.getElementById("paramInput")
-
+const searchParameter = document.getElementById("paramInput");
+const form = document.getElementById("form");
+let request;
 async function init(){
     try{
         if(!window.sessionStorage.getItem("APIKey")){
             await popUpAPI();
         }
         
-        switch(searchType.value){
-            case "title":
-                const request = await apiHandler.getAnimeByName(paramInput.value);
-                break;
-        }
-
-        const request = await apiHandler.getAnimeByName("Death note");
-        
-        const template = document.getElementById("anime-card");
-        const container = document.getElementById("div-anime-cards");
-
-
-        
-        request.forEach(anime => {
-            
-            const animeCard = template.content.cloneNode(true);
-            animeCard.querySelector('.anime-card-title').textContent = anime.title;
-            animeCard.querySelector('.anime-card-img').src = anime.image;
-            animeCard.querySelector('.anime-card-synopsis').textContent = "Synopsis : "+anime.synopsis;
-            animeCard.querySelector('.anime-card-genre').textContent = "Genre : "+anime.genres[0];
-            animeCard.querySelector('.anime-card-rank').textContent = "Rang : "+anime.rank;
-            animeCard.querySelector('.anime-card-episode-number').textContent = "Episodes : "+ anime.episodes;
-            animeCard.querySelector('.anime-card-id').textContent = "Id : "+anime._id;
-        
-            container.appendChild(animeCard);
-
-        });
     }catch (error){
         init();
     }
@@ -44,3 +18,49 @@ async function init(){
 init();
 
 
+form.addEventListener("submit", displayAnime);
+
+async function displayAnime(event){
+    event.preventDefault();
+    console.log("searchType =", searchType.value);
+    switch(searchType.value){
+        case "title":
+            console.log(paramInput.value);
+            request = await apiHandler.getAnimeByName(searchParameter.value);
+            break;
+        case "genre":
+            console.log(paramInput.value);
+            // request = await apiHandler.getAnimeBy...(paramInput.value);
+        break;
+        case "id":
+            console.log(paramInput.value);
+            //request = await apiHandler.getAnimeById(paramInput.value);
+        break;
+        case "rank":
+            console.log(paramInput.value);
+            //request = await apiHandler.getAnimeByRank(paramInput.value);
+        break;
+    }
+
+    //request = await apiHandler.getAnimeByName("Death note");
+
+    const template = document.getElementById("anime-card");
+    const container = document.getElementById("div-anime-cards");
+    console.log(request);
+
+
+    request.forEach(anime => {
+        
+        const animeCard = template.content.cloneNode(true);
+        animeCard.querySelector('.anime-card-title').textContent = anime.title;
+        animeCard.querySelector('.anime-card-img').src = anime.image;
+        animeCard.querySelector('.anime-card-synopsis').textContent = anime.synopsis;
+        animeCard.querySelector('.anime-card-genre').textContent = anime.genres[0];
+        animeCard.querySelector('.anime-card-rank').textContent = anime.ranking;
+        animeCard.querySelector('.anime-card-episode-number').textContent = anime.episodes;
+        animeCard.querySelector('.anime-card-id').textContent = anime._id;
+
+        container.appendChild(animeCard);
+
+    })
+};
