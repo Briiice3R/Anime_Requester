549b27a748e558d23391d089a93fa5254058931b
BriceHarper <58884670+BriceHarper@users.noreply.github.com>
2025-10-13 08:37:52 +0200

    fix: Fix the darkMode issue

--- src/js/main.js
+++ src/js/main.js
@@ -105,3 +105,37 @@ function displayAnime() {
         
 
 }
+
+
+const body = document.querySelector("body");
+const themeButton = document.querySelector(".themeButton");
+const iconTheme = document.getElementById("themeIcon");
+let darkMode = false;
+window.addEventListener("load", ()=>{
+    if(window.sessionStorage.getItem("darkMode") == "yes"){
+        body.classList.add("dark");
+        iconTheme.classList.remove("fa-moon");
+        iconTheme.classList.add("fa-sun");
+        darkMode = true;
+    } else{
+        body.classList.remove("dark");
+        iconTheme.classList.remove("fa-sun");
+        iconTheme.classList.add("fa-moon");
+        darkMode = false;
+    }
+});
+themeButton.addEventListener("click", ()=>{
+    if(!darkMode){
+        body.classList.add("dark");
+        iconTheme.classList.remove("fa-moon");
+        iconTheme.classList.add("fa-sun");
+        darkMode = true;
+        window.sessionStorage.setItem("darkMode", "yes")
+    } else{
+        darkMode = false;
+        body.classList.remove("dark");
+        iconTheme.classList.remove("fa-sun");
+        iconTheme.classList.add("fa-moon");
+        window.sessionStorage.setItem("darkMode", "false")
+    }
+});
